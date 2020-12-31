a80fecb7d1ca43f52264872025c108af4e5ff668
vjiki <nezzabudka5@gmail.com>
2020-12-31 15:50:36 +0300

    empty project with spring boot

---
+++ src/main/java/com/github/vjiki/wildsql/WildsqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.vjiki.wildsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildsqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vjiki/wildsql/WildsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vjiki.wildsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
