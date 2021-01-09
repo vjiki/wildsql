c528a0596e72d25f9f77cfd9998c3491c90d410d
vjiki <nezzabudka5@gmail.com>
2021-01-09 15:46:35 +0300

    remove string descriptions from enums

--- src/main/java/com/github/vjiki/wildsql/models/AnimalClass.java
+++ src/main/java/com/github/vjiki/wildsql/models/AnimalClass.java
@@ -1,7 +1,8 @@
 package com.github.vjiki.wildsql.models;
 
-
+// @TODO rename class with better name
 public enum AnimalClass {
+
     AGNATHA,
     CHRONDRICHTYES,
     OSTEICHTHYES,
@@ -10,36 +11,5 @@ public enum AnimalClass {
     AVES,
     MAMMALIA
     ;
-
-    /*AGNATHA("AGNATHA"),
-    CHRONDRICHTYES("Chrondrichtyes (cartilaginous fish)"),
-    OSTEICHTHYES("Osteichthyes (bony fish)"),
-    AMPHIBIA("Amphibia (amphibians)"),
-    REPTILIA("Reptilia (reptiles)"),
-    AVES("Aves  (birds)"),
-    MAMMALIA("Mammalia (mammals)")
-    ;
-
-    private final String name;
-
-    AnimalClass(final String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public static final String[] STRINGS = Arrays.stream(AnimalClass.values())
-            .map(Enum::name)
-            .collect(Collectors.toList())
-            .toArray(new String[0]);
-
-     */
 
 }
--- src/main/java/com/github/vjiki/wildsql/models/GroupOfPopulation.java
+++ src/main/java/com/github/vjiki/wildsql/models/GroupOfPopulation.java
@@ -9,22 +9,4 @@ public enum GroupOfPopulation {
     ENDANGERED_TYPE
     ;
 
-    /*
-    OVERPOPULATION("Overpopulation"),
-    ABOVE_NORMAL("Above normal"),
-    NORMAL("Normal"),
-    BELOW_NORMAL("Below normal"),
-    ENDANGERED_TYPE("Endangered type")
-    ;
-
-    private final String text;
-
-    GroupOfPopulation(final String text) {
-        this.text = text;
-    }
-
-    @Override
-    public String toString() {
-        return text;
-    }*/
 }
